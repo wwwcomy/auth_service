eab646a70464b21524f4cd7cd120da2c7168614f
wwwcomy <wwwcomy@gmail.com>
2018-06-22 23:27:53 +0800

    Add EntityListeners, seems weird, audit can work without EntityListeners

--- auth-service-domain/src/main/java/com/iteye/wwwcomy/authservice/entity/BasePersistedObject.java
+++ auth-service-domain/src/main/java/com/iteye/wwwcomy/authservice/entity/BasePersistedObject.java
@@ -3,6 +3,7 @@ package com.iteye.wwwcomy.authservice.entity;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
@@ -14,6 +15,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
@@ -28,6 +30,7 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
  *
  */
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BasePersistedObject {
 
     @JsonProperty(access = Access.READ_ONLY)
